e822bcf6974f024bd662e02457e6afadee5512ec
andber6 <andre.bergan99@hotmail.com>
2020-10-30 09:15:55 +0000

    added shopping bag page, styling and logic

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+  
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
@@ -20,18 +21,16 @@ def all_products(request):
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey == 'lower_name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
