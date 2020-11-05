6f9836fa58af23470a726b830b0dae5c1821fb24
andber6 <andre.bergan99@hotmail.com>
2020-11-05 13:42:53 +0000

    working on stripe design etc

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,7 @@
 /*
-    Core logic/payment flow for this come from here:
+    Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-
-    CSS from here:
+    CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
@@ -10,8 +9,6 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-var card = elements.create('card');
-
 var style = {
     base: {
         color: '#000',
@@ -24,8 +21,8 @@ var style = {
     },
     invalid: {
         color: '#dc3545',
-        iconColor: 'dc3545'
+        iconColor: '#dc3545'
     }
 };
-
-card.amount('#card-element', {style: style});
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
