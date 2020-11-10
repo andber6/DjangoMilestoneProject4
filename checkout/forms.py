5a4ea3920c5891bb257595fb6b6588cd082e6aa7
andber6 <andre.bergan99@hotmail.com>
2020-11-10 14:14:01 +0000

    added checkout success logic

--- checkout/forms.py
+++ checkout/forms.py
@@ -6,13 +6,13 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                    'street_address1', 'street_address2',
-                    'town_or_city', 'postcode', 'country',
-                    'county',)
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholder and classes, remove auto-generated
+        Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
