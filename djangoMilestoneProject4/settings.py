55e2e2d76936d721c315983e022542db6728b672
andber6 <andre.bergan99@hotmail.com>
2020-11-20 11:59:27 +0000

    fixing register account issue

--- djangoMilestoneProject4/settings.py
+++ djangoMilestoneProject4/settings.py
@@ -210,7 +210,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'management@mystore.com'
+    DEFAULT_FROM_EMAIL = 'andre.bergan99@hotmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
